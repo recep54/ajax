a0564d0a4dd68961dc1b89878353c85b6f91b079
recep54 <recep54@outlook.fr>
2018-11-23 01:34:46 +0100

    sa marche

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+require 'faker'
+
+15.times do
+  Email.create(
+    object: Faker::GreekPhilosophers.quote,
+    body:Faker::VForVendetta.speech,
+    read:false
+    )
+end
